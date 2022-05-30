799e4c3380be119d8c489c46a1e3d7889abef2fe
Flo <floriangst1998@gmail.com>
2022-05-30 18:31:25 +0200

    initial commit

---
+++ scripts/js/menu.js
@@ -0,0 +1,8 @@
+
+function openNav(){
+    document.getElementById("sidemenu").style.display = "block";
+}
+
+function closeNav(){
+    document.getElementById("sidemenu").style.display = "none";
+}
